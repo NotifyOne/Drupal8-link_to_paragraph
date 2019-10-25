6465ae013a8d72b7485e2165e7e85021cfe38f7b
NotifyOne <anton220812@gmail.com>
2019-10-25 15:06:15 +0300

    WIP #8: Create base form

---
+++ src/Form/ConfigurateForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\link_to_paragraph\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements a simple form.
+ */
+class ConfigurateForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    // TODO: Implement getFormId() method.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // TODO: Implement buildForm() method.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // TODO: Implement submitForm() method.
+  }
+
+}
